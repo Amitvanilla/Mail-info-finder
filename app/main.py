7212a00a9689592cd460cb007a2b0999427e722f
Amitvanilla <59921831+Amitvanilla@users.noreply.github.com>
2024-09-16 19:43:28 +0530

    cors added

--- app/main.py
+++ app/main.py
@@ -2,10 +2,20 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from app.openai_service import get_openai_response
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],  
+)
+
 
 class RequestBody(BaseModel):
     prompt: str
@@ -13,7 +23,6 @@ class RequestBody(BaseModel):
 @app.post("/generate/")
 async def generate_description(request_body: RequestBody):
     prompt = request_body.prompt
-    print(prompt) 
     try:
         response = await get_openai_response(prompt)
         return {"response": response}
